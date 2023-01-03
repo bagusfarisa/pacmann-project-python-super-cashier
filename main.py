f8cd9b9ffa5342008f5bfc7657c1b0b9e54bcc48
Guntur <58203532+bagusfarisa@users.noreply.github.com>
2023-01-03 18:47:10 +0700

    Add print function to show the transaction Dict

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ print("2. 'Pasta Gigi', Qty: 3, Harga: 15000\n")
 
 trnsct_123.add_item("Ayam Goreng", 2, 20000)
 trnsct_123.add_item("Pasta Gigi", 3, 15000)
+print(trnsct_123.order_items)
+print()
 trnsct_123.show_order_table()
 
 
 # Test Case 2
-print("\n>> TEST CASE 2")
+print("\n\n>> TEST CASE 2")
 print("Ternyata customer salah menambahkan item dan ingin menghapus item 'Pasta Gigi' dari belanjaan.\n")
 
 trnsct_123.delete_item("Pasta Gigi")
+print(trnsct_123.order_items)
+print()
 trnsct_123.show_order_table()
 
 # Test Case 3
-print("\n>> TEST CASE 3")
+print("\n\n>> TEST CASE 3")
 print("Ternyata customer salah memasukkan item yang lain juga.")
 print("Daripada menghapus satu persatu, dia ingin me-reset seluruh daftar belanjaan.\n")
 
@@ -29,7 +33,7 @@ trnsct_123.reset_transaction()
 trnsct_123.show_order_table()
 
 # Test Case 4
-print("\n>> TEST CASE 4")
+print("\n\n>> TEST CASE 4")
 print("Customer akhirnya memutuskan untuk membeli 4 barang berikut ini:")
 print("1. 'Ayam Goreng', Qty: 2, Harga: 20000")
 print("2. 'Pasta Gigi', Qty: 3, Harga: 15000")
@@ -40,7 +44,7 @@ trnsct_123.add_item("Ayam Goreng", 2, 20000)
 trnsct_123.add_item("Pasta Gigi", 3, 15000)
 trnsct_123.add_item("Mainan Mobil", 1, 200000)
 trnsct_123.add_item("Mie Instant", 5, 3000)
-
+print(trnsct_123.order_items)
 print()
 trnsct_123.show_order_table()
 print()
